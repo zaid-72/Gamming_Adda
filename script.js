28d18f27ffa028e6a04c1a3f9d55ecf9e9fc03ad
zaid <zaidsyed7264@gmail.com>
2024-11-16 10:51:44 +0530

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function abc(){
+    document.getElementById('up_bottom3').style ='overflow-x:scroll;'
+    document.getElementById('op').style = "display:block;"
+
+  }
+
+  function a(){
+    document.getElementById('up_bottom3').style = "overflow-x: hidden;"
+    document.getElementById('op').style = "display:none;"
+  }
